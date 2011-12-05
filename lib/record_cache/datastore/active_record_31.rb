1760eefd5d5b2be1103d01d5021808ece1575569
Bryan Mundie <blmundie@gmail.com>
2011-12-05 17:18:21 -0500

    fixed call for has_one delete methods

--- lib/record_cache/datastore/active_record_31.rb
+++ lib/record_cache/datastore/active_record_31.rb
@@ -341,14 +341,14 @@ module RecordCache
       end
 
       module InstanceMethods
-        def delete_with_record_cache(method)
+        def delete_with_record_cache(method = options[:dependent])
           # invalidate :id cache for all record
           if load_target
             target.class.record_cache.invalidate(record.id) if target.class.record_cache? unless target.new_record?
           end
           # invalidate the referenced class for the attribute/value pair on the index cache
           @reflection.klass.record_cache.invalidate(@reflection.foreign_key.to_sym, @owner.id) if @reflection.klass.record_cache?
-          delete_without_record_cache(records, method)
+          delete_without_record_cache(method)
         end
       end
     end
